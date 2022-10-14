1cc1bc83cb2e69ca2c8dcb117b8a1c2b8d7b7134
kpsherva <karolina.m.przerwa@gmail.com>
2022-10-14 09:02:25 +0200

    global: downgrade eslint webpack plugin
    * remove deprecated yaml.safeLoad
    * upgrade js-yaml

--- prettier.js
+++ prettier.js
@@ -2,4 +2,4 @@ const fs = require("fs");
 const path = require("path");
 const yaml = require("js-yaml");
 
-module.exports = yaml.safeLoad(fs.readFileSync(path.join(__dirname, "configs/prettier.yaml"), "utf8"));
+module.exports = yaml.safe(fs.readFileSync(path.join(__dirname, "configs/prettier.yaml"), "utf8"));
