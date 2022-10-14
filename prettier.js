4ac0c5008521e624d899f27f49b9956e317168df
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-10-14 12:39:41 +0200

    fix: wrong function name
    * use load instead of safeLoad or safe
    
    * use detect instead of latest. latest is for newer versions

--- prettier.js
+++ prettier.js
@@ -2,4 +2,4 @@ const fs = require("fs");
 const path = require("path");
 const yaml = require("js-yaml");
 
-module.exports = yaml.safe(fs.readFileSync(path.join(__dirname, "configs/prettier.yaml"), "utf8"));
+module.exports = yaml.load(fs.readFileSync(path.join(__dirname, "configs/prettier.yaml"), "utf8"));
